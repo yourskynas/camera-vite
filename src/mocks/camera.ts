0c5dcbc467be98f7dbc21cffd3d1ae62fe44744a
yourskynas <nastiazaharova69@gmail.com>
2024-10-03 17:01:32 +0300

    Добавляет моковые данные

---
+++ src/mocks/camera.ts
@@ -0,0 +1,16 @@
+export const camera = {
+  id: 1,
+  name: 'Ретрокамера Dus Auge lV',
+  vendorCode: 'DA4IU67AD5',
+  type: 'Коллекционная',
+  category: 'Видеокамера',
+  description: 'Немецкий концерн BRW разработал видеокамеру Das Auge IV в начале 80-х годов, однако она до сих пор пользуется популярностью среди коллекционеров и яростных почитателей старинной техники.',
+  level: 'Нулевой',
+  price: 65000,
+  rating: 5,
+  reviewCount: 16,
+  previewImg: 'img/content/das-auge.jpg',
+  previewImg2x: 'img/content/das-auge@2x.jpg',
+  previewImgWebp: 'img/content/das-auge.webp',
+  previewImgWebp2x: 'img/content/das-auge@2x.webp'
+};
---
+++ src/mocks/reviews.ts
@@ -0,0 +1,52 @@
+export const reviews = [
+  {
+    id: '5f3531a5-5024-447c-840c-b329d0c6d78d',
+    userName: 'Павел',
+    advantage: 'Рекомендую данный аппарат',
+    disadvantage: 'Без объектива',
+    review: 'Приобрела камеру ориентируясь на отзывы, но выявила множество несоответствий. Цена не оправдала ожидания',
+    rating: 3,
+    createAt: '2024-03-13T10:05:34.532Z',
+    cameraId: 1
+  },
+  {
+    id: 'a03b48b3-6265-481e-83ae-b0f36a1ae038',
+    userName: 'Ольга',
+    advantage: 'Легкая в плане веса, удобная в интерфейсе, зарядка',
+    disadvantage: 'Плохая камера, не рекомендую',
+    review: 'Это моя первая камера. Я в восторге, нареканий нет',
+    rating: 1,
+    createAt: '2024-02-15T10:05:34.564Z',
+    cameraId: 1
+  },
+  {
+    id: 'a8d4bcc6-e39f-4175-8abb-e47a6bd23b14',
+    userName: 'Кирилл',
+    advantage: 'Хороший стабилизатор и матрица',
+    disadvantage: 'Тяжелая. Рука быстро устаёт',
+    review: 'Не возможно найти дополнительные аккамуляторы. К сожалению, те, что идут в комплекте не держут более 7 часов?',
+    rating: 5,
+    createAt: '2024-05-10T10:05:34.564Z',
+    cameraId: 1
+  },
+  {
+    id: 'b2e7af59-36b0-41f9-aa2a-2254f152d801',
+    userName: 'Ольга',
+    advantage: 'Недорогая, за такую цену отличный вариант.',
+    disadvantage: 'Быстро садиться зарядка',
+    review: 'В целом для домашнего использования в самый раз!',
+    rating: 5,
+    createAt: '2024-03-19T10:05:34.564Z',
+    cameraId: 1
+  },
+  {
+    id: '2f723bf1-89ce-4bce-965e-2d9d2380745e',
+    rating: 5,
+    userName: 'jkhjk',
+    advantage: 'jkhjj',
+    disadvantage: 'khjk',
+    review: 'hkjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+    cameraId: 1,
+    createAt: '2024-07-30T11:21:04.205Z'
+  }
+];
