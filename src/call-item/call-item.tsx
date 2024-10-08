0b76531d7271f840143b4c3b4a54eaab7277a316
yourskynas <nastiazaharova69@gmail.com>
2024-10-08 06:25:07 +0300

    Добавляет валидацию для поля с телефоном

--- src/call-item/call-item.tsx
+++ src/call-item/call-item.tsx
@@ -1,20 +1,81 @@
+import { useRef, useState } from 'react';
 import { CameraType } from '../types';
+
+const STYLE_ERROR = {
+  color: 'red',
+  marginTop: 0,
+  fontSize: 12,
+  fontWeight: 300
+};
+
+type ErrorItemProps = {
+  message: string;
+}
 
 type CallItemProps = {
   activeProduct: CameraType;
   onClick: (camera: null) => void;
 }
 
+type PhoneType = string | undefined;
+
+const ErrorItem = ({message}: ErrorItemProps): JSX.Element => (
+  <div>
+    <p style={STYLE_ERROR}>{message}</p>
+  </div>
+);
+
 const CallItem = ({activeProduct, onClick}: CallItemProps): JSX.Element => {
+  const phoneRef = useRef<HTMLInputElement | null>(null);
+  const [errorText, setErrorText] = useState<string | null>(null);
+  const [formatedPhone, setFormatedPhone] = useState<string | null>(null);
+  const [isError, setIsError] = useState<boolean>(false);
+
   const { name, vendorCode, previewImgWebp, previewImgWebp2x, previewImg, previewImg2x, category, level, price } = activeProduct;
 
   const closePopup = () => {
     onClick(null);
   };
 
+  const convertPhone = (phone: PhoneType) => {
+    let newPhone = '';
+    if (phone) {
+      newPhone = `+7${phone.split(/[^0-9]/g).join('').slice(1,11)}`;
+      setFormatedPhone(newPhone);
+    }
+
+  };
+
+  const validatePhone = (phone: PhoneType) => {
+    const digitRegex = /^(\+7|8)(\s|\()?\d{3}(\s|\))?\d{3}(\s|-)?\d{2}(\s|-)?\d{2}$/;
+    const message = 'Введите телефон в формате: +7(9XX)XXX-XX-XX или 89XXXXXXXXX';
+
+    setIsError(false);
+    setErrorText(null);
+
+    if (phone && !digitRegex.test(phone)) {
+      setIsError(true);
+      return setErrorText(message);
+    }
+
+    convertPhone(phone);
+  };
+
   const handleKeyDown = (evt: React.KeyboardEvent<HTMLDivElement>) => {
     if (evt.key === 'Escape') {
       closePopup();
+    }
+  };
+
+  const handleInputChange = (phone: PhoneType) => {
+    validatePhone(phone);
+  };
+
+  const handleButtonClick = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    evt.preventDefault();
+    // следующая конструкция, чтобы не ругался линтер, удалить после использования переменной
+    if (formatedPhone) {
+      return formatedPhone;
     }
   };
 
@@ -49,12 +110,13 @@ const CallItem = ({activeProduct, onClick}: CallItemProps): JSX.Element => {
                   <use xlinkHref="#icon-snowflake"></use>
                 </svg>
               </span>
-              <input type="tel" name="user-tel" placeholder="Введите ваш номер" required onKeyDown={(evt) => handleKeyDown(evt)} />
+              <input type="tel" name="user-tel" placeholder="Введите ваш номер" ref={phoneRef} required onKeyDown={(evt) => handleKeyDown(evt)} onChange={() => handleInputChange(phoneRef.current?.value)} />
             </label>
             <p className="custom-input__error">Нужно указать номер</p>
           </div>
+          {errorText !== null && <ErrorItem message={errorText} />}
           <div className="modal__buttons">
-            <button className="btn btn--purple modal__btn modal__btn--fit-width" type="button">
+            <button className="btn btn--purple modal__btn modal__btn--fit-width" onClick={(evt) => handleButtonClick(evt)} type="button" disabled={isError}>
               <svg width="24" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-add-basket"></use>
               </svg>Заказать
