02b4822525e13ea65c33ba375374907c2f136911
yourskynas <nastiazaharova69@gmail.com>
2024-10-10 16:52:54 +0300

    Модальное окно для обратной связи
    Поднимает логику показа попапа на уровень app и template-page, тк оно используется на нескольких страницах

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,18 +4,39 @@ import { AppRoute } from '../../constants';
 import ProductPage from '../../product-page/product-page';
 import TemplatePage from '../../template-page/template-page';
 import NotFoundPage from '../../not-found-page/not-found-page';
+import { useEffect, useState } from 'react';
+import { CameraType } from '../../types';
 
-const App = (): JSX.Element => (
-  <BrowserRouter>
-    <Routes>
-      <Route element={<TemplatePage />}>
-        <Route index element={<Navigate to={AppRoute.Camera} />} />
-        <Route path={AppRoute.Catalog} element={<CatalogPage />} />
-        <Route path={AppRoute.Camera} element={<ProductPage />} />
-        <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+const App = (): JSX.Element => {
+  const [ isActiveModal, setIsActiveModal ] = useState<boolean>(false);
+  const [ activeProduct, setActiveProduct ] = useState<CameraType | null>();
+  const handleButtonClick = (camera: CameraType | null) => {
+    setIsActiveModal(!isActiveModal);
+    setActiveProduct(camera);
+  };
+
+  useEffect(() => {
+    if (isActiveModal) {
+      document.body.classList.add('scroll-lock');
+    }
+
+    return () => {
+      document.body.classList.remove('scroll-lock');
+    };
+  }, [isActiveModal]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<TemplatePage activeProduct={activeProduct} isActiveModal={isActiveModal} onClick={handleButtonClick} />}>
+          <Route index element={<Navigate to={AppRoute.Catalog} />} />
+          <Route path={AppRoute.Catalog} element={<CatalogPage onClick={handleButtonClick} />} />
+          <Route path={AppRoute.Camera} element={<ProductPage onClick={handleButtonClick} />} />
+          <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/template-page/template-page.tsx
+++ src/template-page/template-page.tsx
@@ -3,14 +3,23 @@ import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 import { AppRoute } from '../constants';
 import Anchor from '../components/anchor/anchor';
+import CallItem from '../components/call-item/call-item';
+import { CameraType } from '../types';
 
-const TemplatePage = (): JSX.Element => {
+type TemplatePageProps = {
+  activeProduct: CameraType | null | undefined;
+  isActiveModal: boolean;
+  onClick: (camera: null) => void;
+}
+
+const TemplatePage = ({activeProduct, isActiveModal, onClick}: TemplatePageProps): JSX.Element => {
   const match = useMatch(AppRoute.Camera);
   return (
     <div className="wrapper">
       <Header />
       <main>
         <Outlet />
+        {isActiveModal && activeProduct && <CallItem activeProduct={activeProduct} onClick={onClick} />}
       </main>
       {match && <Anchor />}
       <Footer />
