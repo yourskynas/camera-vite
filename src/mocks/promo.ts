44b0d632768c7dbfa38021de437ff702d16aaa53
yourskynas <nastiazaharova69@gmail.com>
2024-10-14 06:54:49 +0300

    Добавляет моковые данные для блока с промо

---
+++ src/mocks/promo.ts
@@ -0,0 +1,26 @@
+export const promo = [
+  {
+    id: 7,
+    name: 'Look 54',
+    previewImg: 'img/content/promo-look-54.jpg',
+    previewImg2x: 'img/content/promo-look-54@2x.jpg',
+    previewImgWebp: 'img/content/promo-look-54.webp',
+    previewImgWebp2x: 'img/content/promo-look-54@2x.webp'
+  },
+  {
+    id: 35,
+    name: 'Click Pro',
+    previewImg: 'img/content/promo_click_pro.jpg',
+    previewImg2x: 'img/content/promo_click_pro@2x.jpg',
+    previewImgWebp: 'img/content/promo_click_pro.webp',
+    previewImgWebp2x: 'img/content/promo_click_pro@2x.webp'
+  },
+  {
+    id: 36,
+    name: 'Click Lite R',
+    previewImg: 'img/content/promo_click-lite-r.jpg',
+    previewImg2x: 'img/content/promo_click-lite-r@2x.jpg',
+    previewImgWebp: 'img/content/promo_click-lite-r.webp',
+    previewImgWebp2x: 'img/content/promo_click-lite-r@2x.webp'
+  }
+];
